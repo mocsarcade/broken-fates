8ee1135d1252f6bef12538fad2eadca8c3e46c19
Jared Taylor <jared.xyz@protonmail.com>
2018-01-31 20:40:21 -0500

    Items, Inventory, and More
    I have added a new class for Items, Inventory Items, and a new Player class. I moved the PlayerMovement class into the new Player class, and have created a List<InventoryItem> in Player. I have also added a method for adding Items to the player inventory, to be used in the future.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    // You should be able to return ID by: nameOfItem.ID <-- You could also set the value by doing --> nameOfItem.ID = 42;
+    public int ID { get; set; }
+    public string itemName { get; set; }
+    public string itemNamePlural { get; set; }
+
+    public Item(int givenID, string aItemName, string aItemNamePlural)
+    {
+        ID = givenID;
+        itemName = aItemName;
+        itemNamePlural = aItemNamePlural;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+    private Animator animator;
+    private bool frozen;
+
+    // Awake starts when the script begins
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        frozen = false;
+    }
+
+	void Update () {
+		// Currently empty
+	}
+
+    //*******************Player Inventory*********************//
+    // For the player inventory, we're going to create a list of items. - JRT
+    public List<InventoryItem> Inventory { get; set; }
+
+    // Inventory accepts two parameters: (Item, Quantity). - JRT
+    public void AddItemToInventory(Item itemToAdd)
+    {
+        foreach (InventoryItem i in Inventory)
+        {
+            // Do they already have the item?
+            if (i.Details.ID == itemToAdd.ID)
+            {
+                // If so:
+                i.Quantity++;
+                return;
+            }
+        }
+        // If they didn't have the item:
+        Inventory.Add(new InventoryItem(itemToAdd, 1));
+    }
+
+
+    //***************Player Movement**********************//
+    protected override void ComputeVelo()
+    {
+        //Movement method of MovingObject acts ever few ticks to move character according to how calcMovement has been changed. It will be 0 on default
+        //Get what keys are being pushed and save into a 2-value vector (Basically just an array with two values).
+        if (frozen == false)
+        {
+            calcMovement.x = Input.GetAxis("Horizontal");
+            calcMovement.y = Input.GetAxis("Vertical");
+
+            //Set Move and MoveX which tell animator what direction our player should be facing
+            animator.SetFloat("Move", -calcMovement.y);
+            animator.SetFloat("MoveX", calcMovement.x);
+        }
+        else
+        {
+            //if the player is using magic, or is frozen, his movement drops to 0
+            calcMovement = Vector2.zero;
+
+            //Animator drops to 0 and player is standing still when player is using magic, or is frozen.
+            animator.SetFloat("Move", 0);
+            animator.SetFloat("MoveX", 0);
+
+        }
+    }
+
+    // animator gets its component every time this script is "enabled". Basically when the script begins.
+    public void Halt()
+    {
+        frozen = true;
+    }
+
+    // animator gets its component every time this script is "enabled". Basically when the script begins.
+    public void Resume()
+    {
+        frozen = false;
+    }
+}
