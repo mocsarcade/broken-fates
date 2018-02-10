e6625feae19b61fa541bac6b9ff907ec1bb99a0f
Jared Taylor <jared.xyz@protonmail.com>
2018-02-10 18:37:21 -0500

    Added functional ItemPickup
    To create an item: add a GameObject to the scene. Then, drag the ItemPickup script to the object. Then, create a new Item asset in the Items folder. Add that to the item GameObject. Congrats!!! You have a new item! Now, you can walk over to the item in the scene, and right click it to pick it up.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    #region Singleton
+    // We're going to create a singleton for Inventory. So, we're going to make a static call of Inventory, so we can only
+    // ever have one instance of Inventory called.
+    // So, you can access this \/ by calling Inventory.instance in another class.
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one inventory existing.");
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public int space = 10;
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (items.Count >= space)
+        {
+            // Send GUI message to Player here (todo), return false.
+            return false;
+        }
+        // Otherwise we can pick up the item, return true.
+        items.Add(item);
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+
+}
---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach ItemPickup component to an Item
+public class ItemPickup : MonoBehaviour {
+
+    public Item item;
+    public float pickupRange = 1;
+
+    // We will need to see where the item and player are in relation to each other
+    // thisItem will take the transform value of the item it is attached to
+    private Transform thisItem;
+    private Transform player;
+
+    private void Awake()
+    {
+        thisItem = this.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && (this.Distance() <= pickupRange))
+        {
+            PickUp();
+        }
+    }
+
+    public void PickUp()
+    {
+        // Add returns true if it an item can be picked up.
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            DestroyObject(gameObject);
+        }
+    }
+
+    public float Distance()
+    {      
+        return Vector3.Distance(thisItem.position, player.position);
+    }
+
+}
