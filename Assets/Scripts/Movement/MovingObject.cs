1858cc023c9a00ca9534434bb8106d45136bff48
kingoftheconnors <Connormcp777@gmail.com>
2018-02-21 19:04:41 -0500

    Fixed Animation and Made Movement much cleaner.
    Character is also slightly faster without the slippery floor effect
    I also moved "frozen" to the superclass, allowing all moving objects to
    have a "frozen" tag. This can be used in later implementation of
    freezing powers.

--- Assets/Scripts/Movement/MovingObject.cs
+++ Assets/Scripts/Movement/MovingObject.cs
@@ -9,13 +9,19 @@ public class MovingObject : MonoBehaviour {
     public float maxVel;
     protected Rigidbody2D rb2d;
 	public Vector2 calcMovement;
-	
+	protected bool frozen;
+
 	// rb2d gets its component every time this script is "enabled". Basically when the script begins.
     void OnEnable()
     {
 		//Initialize RigidBody, which will move the object when it is moved
         rb2d = GetComponent<Rigidbody2D>();
     }
+
+	void Awake()
+	{
+		frozen = false;
+	}
 	
 	//Every update, the players' movement will be calculated using the ComputeVelo method of the inheriting objects
 	void Update() {
@@ -26,16 +32,29 @@ public class MovingObject : MonoBehaviour {
 	//Every fixed update, the player will move. This will keep the players' movements uniform regardless of computer lag
     void FixedUpdate () {
 		//We don't want our objects to go too fast, so first we must check that the objects' velocity hasn't grown too high
-		if(Mathf.Abs(rb2d.velocity.x) < maxVel && Mathf.Abs(rb2d.velocity.y) < maxVel) {
+		if(calcMovement.magnitude< maxVel && calcMovement.magnitude > 0.1 && frozen == false) {
 			//Move the rigidbody by applying force, and the object will move too
-            rb2d.AddForce(calcMovement * speed);
-        }
+			rb2d.AddForce(calcMovement * speed);//, ForceMode.VelocityChange);
+			//Add force to move our character. VelocityChange ignores mass to remove stopping latency
+		}
     }
 	
 	//The "virtual" is important to show this method will be overriden
 	//This is the method that will be overriden by findVelo in any method that inherits this class. This is how all moving objects decide their move patterns
 	protected virtual void ComputeVelo () {
 		
+	}
+
+	// When a script is called or time is stopped, the player will have to freeze
+	public void Halt()
+	{
+		frozen = true;
+	}
+
+	// Let player move again
+	public void Resume()
+	{
+		frozen = false;
 	}
 
 }
--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -8,50 +8,35 @@ public class PlayerMovement : MovingObject
 {
 
     private Animator animator;
-    private bool frozen;
 
     // animator gets its component every time this script is "enabled". Basically when the script begins.
     void Awake()
     {
         animator = GetComponent<Animator>();
-        frozen = false;
     }
 
     protected override void ComputeVelo()
     {
         //Movement method of MovingObject acts ever few ticks to move character according to how calcMovement has been changed. It will be 0 on default
         //Get what keys are being pushed and save into a 2-value vector (Basically just an array with two values).
-        if (frozen == false)
-        {
-            calcMovement.x = Input.GetAxis("Horizontal");
-            calcMovement.y = Input.GetAxis("Vertical");
+			calcMovement = (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
 
-            //Set Move and MoveX which tell animator what direction our player should be facing
-            animator.SetFloat("Move", -calcMovement.y);
-            animator.SetFloat("MoveX", calcMovement.x);
-        }
-        else
-        {
-            //if the player is using magic, or is frozen, his movement drops to 0
-            calcMovement = Vector2.zero;
-
-            //Animator drops to 0 and player is standing still when player is using magic, or is frozen.
-            animator.SetFloat("Move", 0);
-            animator.SetFloat("MoveX", 0);
-
-        }
+            //Set Move and MoveX which tell animator what direction our player should be facing (unless player is idle, in which case direction will be saved for animator
+			if(calcMovement.magnitude > 0.1 && frozen == false)
+				{
+					if(animator.GetBool("Moving") == false)
+					{
+						animator.SetBool("Moving", true);
+					}
+					animator.SetFloat("MoveY", calcMovement.y);
+					animator.SetFloat("MoveX", calcMovement.x);
+				}
+				else
+				{
+					if(animator.GetBool("Moving") == true)
+					{
+					animator.SetBool("Moving", false);
+					}
+				}
     }
-
-    // animator gets its component every time this script is "enabled". Basically when the script begins.
-    public void Halt()
-    {
-        frozen = true;
-    }
-
-    // animator gets its component every time this script is "enabled". Basically when the script begins.
-    public void Resume()
-    {
-        frozen = false;
-    }
-
 }
