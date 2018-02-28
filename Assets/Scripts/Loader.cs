9e858f193d035a6f95796d000822801577d510a1
kingoftheconnors <Connormcp777@gmail.com>
2018-02-27 21:08:31 -0500

    Did some primary work on GameManager
    Worked on loader to automatically put dialogue text vars into
    gamemanager when it's instantiated.

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Loader : MonoBehaviour {
 
 	public GameObject gameManager;
+	public Text subNameText;
+	public Text subDialogueText;
 
 	// Use this for initialization
 	void Awake () {
-		if (GameManager.instance == null)
-			Instantiate (gameManager);
+		if (GameManager.instance == null) {
+			GameObject gameController = Instantiate (gameManager);
+			gameController.GetComponent<DialogueManager>().nameText = subNameText;
+			gameController.GetComponent<DialogueManager>().dialogueText = subDialogueText;
+		}
 	}
 }
