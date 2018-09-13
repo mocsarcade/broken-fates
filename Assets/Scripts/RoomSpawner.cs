fe4f7275444f630d9bc219a94c48f528552634dd
Salp <tyler.bigdog@gmail.com>
2018-09-13 17:48:27 -0400

    RDG is a work in progress. Putting it up so that other people can see.

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int openingDir;
+    //1 --> needs Bottom Door
+    //2 --> needs Left Door
+    //3 --> needs Top Door
+    //4 --> needs Right door
+
+    private RoomTemplate template;
+    private int rand;
+    private bool spawned = false;
+
+    void Start () {
+        template = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplate>();
+        Invoke("Spawn", 0.1f);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Spawn() {
+        if(spawned == false)
+        {
+             if (openingDir == 1)
+             {
+                rand = Random.RandomRange(0, template.bottomRooms.Length);
+                Instantiate(template.bottomRooms[rand], transform.position, template.bottomRooms[rand].transform.rotation);
+             }
+            else if (openingDir == 2)
+            {
+                rand = Random.RandomRange(0, template.leftRooms.Length);
+                Instantiate(template.leftRooms[rand], transform.position, template.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDir == 3)
+            {
+                rand = Random.RandomRange(0, template.topRooms.Length);
+                Instantiate(template.topRooms[rand], transform.position, template.topRooms[rand].transform.rotation);
+            }
+            else if (openingDir == 4)
+            {
+                rand = Random.RandomRange(0, template.rightRooms.Length);
+                Instantiate(template.rightRooms[rand], transform.position, template.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+       
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned == true)
+            Destroy(gameObject);
+    }
 }
---
+++ Assets/Scripts/RoomTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour {
+    public GameObject[] topRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
